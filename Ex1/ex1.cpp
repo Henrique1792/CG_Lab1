3d1bd50cd7bab2dc4b823307c5ab9102dd50f092
Henrique Freitas <freitash1792@gmail.com>
2018-03-21 18:30:53 -0300

    Division of General Calls on Ex1

--- Ex1/ex1.cpp
+++ Ex1/ex1.cpp
@@ -2,14 +2,33 @@
 #include <iostream>
 #include <GL/glut.h>
 
+/*
+ * Henrique F. M. Freitas - 8937225.
+ * Ex: Set DDR algorithm for creating lines using dots.
+ *
+ *
+ * */
 //Global Variables
 
 GLint WINDOW_WIDTH=600,
         WINDOW_HEIGHT=600;
 
+//Starting up Directives
 void init();
 void draw_test();
 
+//Mouse Directives
+void mouseFreeMove(GLint x, GLint y);
+void mouseDragMove(GLint x, GLint y);
+void mouseClickAction(GLint button,GLint action, GLint x, GLint y);
+
+//Keyboard Directives
+void keyboardRegularKey(GLint key, GLint x, GLint y);
+void keyboardSpecialKey(GLint key, GLint x, GLint y);
+
+//Menu Directives
+void rightClickMenu_Content(GLint item_number);
+void rightClickMenu();
 
 int main(int argc, char *argv[]){
         glutInit(&argc, argv);
