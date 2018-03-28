1291222fc3b9f7bc3822b8cadc769c6c57d18008
Henrique Freitas <freitash1792@gmail.com>
2018-03-28 16:41:34 -0300

    using global points and some drawing tries

--- Ex1/src/events.c
+++ Ex1/src/events.c
@@ -1,18 +1,34 @@
 #include "../include/events.h"
 
-
-int extern xMouse,yMouse;
-
+//Extern variables
+int extern xMouse,yMouse, sMouse;
+eMode extern menuOption;
+DOT extern p1, p2;
 /*
  * Events - Mouse
  *
  *
 */
+eMode extern currentMode;
 void onClick(int bt, int state, int x, int y){
-	xMouse=x,yMouse=y;
-	if(bt==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
-		printf("Clicked at %d %d",xMouse, yMouse);
+	xMouse=x, yMouse=y;
+	if(bt==GLUT_LEFT_BUTTON && state==GLUT_DOWN){
+		switch(menuOption){
+			case(DRAW_LINE):
+				p1.x=xMouse, p1.y=y;
+				printf("P1: %d %d\n", p1.x, p1.y);
+				break;
+		}
+		
+	}
+}
 
+void OnMouseMove(int x, int y) {
+	if(sMouse==GLUT_DOWN) {
+		p2.x = x;
+		p2.y=VIEWPORT_Y-y;
+		glutPostRedisplay();
+	}
 }
 /*
  * Events - Keyboard
