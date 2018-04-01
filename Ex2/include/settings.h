874eef034fe91e2b5b22efce8df678ae9f481c66
Henrique Freitas <freitash1792@gmail.com>
2018-04-01 19:02:14 -0300

    settings on Ex2

---
+++ Ex2/include/settings.h
@@ -0,0 +1,50 @@
+#ifndef SETTINGS_H_
+#define SETTINGS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <GL/glut.h>
+
+/*
+ *
+ * Defines goes here!
+ *
+ *
+*/
+
+#define SCREEN_SIZEX 1366
+#define SCREEN_SIZEY 768
+#define VIEWPORT_X 800
+#define VIEWPORT_Y 600
+#define ORTHO_X 400
+#define ORTHO_Y 300
+
+typedef enum dMode{
+	drawIDLE,
+	drawLINE,
+	drawTRI,
+	drawSQUA,
+	drawCIRC,
+	drawELIP 
+}dMode;
+
+typedef struct DOT{
+	int x, y;
+
+}DOT;
+/*
+ * Globals Goes here!
+ *
+*/
+
+
+dMode currentMode;
+int xMouse, yMouse, sMouse,bMouse;
+int lockP1, lockLine;
+int dragging;
+DOT *p1, *p2;
+
+/*
+ * inits
+*/
+#endif
